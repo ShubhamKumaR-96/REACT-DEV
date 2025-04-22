759a0e155f2341d09a09703c08e6337107ec771b
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-04-22 07:41:33 +0530

    css part completed

--- 04.countdown/src/components/Countdown.jsx
+++ 04.countdown/src/components/Countdown.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-import './Countdown.css'
+import React from "react";
+import "./Countdown.css";
 
 const Countdown = () => {
   return (
-    <>
-      <h2>Countdown Timer App</h2>
-      <div className='input'>
-        <input type="datetime-local"  className='datetime'/>
-        <button className='submit'>Start</button>
+    <div className="countdown-container">
+      <h2>Countdown Timer</h2>
+      <div className="input-container">
+        <input type="datetime-local" className="datetime-input" />
+        <button className="submit-btn">Start</button>
       </div>
-    </>
-  )
-}
+      <div className="timer-display">
+        <ul className="timer-list">
+          <li className="timer-item">
+            <span className="timer-value" id="days">00</span>
+            <span className="timer-label">Days</span>
+          </li>
+          <li className="timer-item">
+            <span className="timer-value" id="hours">00</span>
+            <span className="timer-label">Hours</span>
+          </li>
+          <li className="timer-item">
+            <span className="timer-value" id="minutes">00</span>
+            <span className="timer-label">Minutes</span>
+          </li>
+          <li className="timer-item">
+            <span className="timer-value" id="seconds">00</span>
+            <span className="timer-label">Seconds</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Countdown
+export default Countdown;
