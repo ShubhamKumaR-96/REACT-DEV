e0a2cf9be5d916d52df258bc4678ed9fbf587d74
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2025-04-29 15:43:04 +0530

    added navbar

--- 05.MovieSearchingApp/src/App.jsx
+++ 05.MovieSearchingApp/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './App.css'
 import MainRoutes from './routes/MainRoutes'
+import Navbar from './components/Navbar'
 
 const App = () => {
   console.log(import.meta.env.VITE_API_KEY)
   return (
     <>
-      Side Nav bar 
+      <Navbar />
       <MainRoutes />
     </>
   )
---
+++ 05.MovieSearchingApp/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar-wrapper">
+      <div>Movie App ▶️ </div>
+      <div className="search-bar">
+        <input type="text" placeholder="what movie you are thinking about..." />
+      </div>
+      <div>Theme</div>
+    </div>
+  );
+};
+
+export default Navbar;
